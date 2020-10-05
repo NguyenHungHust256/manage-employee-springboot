6687d57a045b64c3b0049405c9497ab52fda0cdc
NguyenHungHust256 <hungpronguyen256@gmail.com>
2020-10-05 11:20:56 +0700

    backend

---
+++ src/main/java/com/example/demomanageemployeeapp/DemoManageEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomanageemployeeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoManageEmployeeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoManageEmployeeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomanageemployeeapp/DemoManageEmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomanageemployeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoManageEmployeeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
